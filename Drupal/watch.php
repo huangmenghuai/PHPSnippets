451de499e8d5927b2c30433db90038c53f57cf34
HUANG Menghuai <linker.huang@gmail.com>
2014-08-14 13:22:28 +0800

    Add watch.

--- Drupal/watch.php
+++ Drupal/watch.php
@@ -1,1 +1,6 @@
 <?php
+// Check that formats are defined.
+if (empty($enabled_methods[$method]['supported_formats']) || !is_array($enabled_methods[$method]['supported_formats'])) {
+  watchdog('rest', 'At least one format must be defined for resource @id', array(':id' => $id), WATCHDOG_ERROR);
+  continue;
+}
